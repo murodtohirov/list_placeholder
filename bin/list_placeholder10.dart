85492c539d51eb540f6f4b2a4d9de1d60f8152e2
Пользователь <murodjontohirov2701@gmail.com>
2022-07-16 13:11:23 +0500

    10

--- bin/list_placeholder10.dart
+++ bin/list_placeholder10.dart
@@ -5,5 +5,22 @@ Argument int n
 Return the List
 
 */
+List func(n) {
+  List l = [];
 
-void main() {}
+  int x = 0;
+  while (x < n) {
+    if (x % 2 == 0) {
+      l.add(x.toString());
+    } else {
+      l.add(x);
+    }
+    x += 1;
+  }
+  print(l[2].runtimeType);
+  return l;
+}
+
+void main() {
+  print(func(4));
+}
